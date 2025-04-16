4b6be5e7ec4f8a8b42b00b670d9846f44e2454c8
Haley Goodfield <haleygoodfield@usf.edu>
2025-04-16 16:34:58 -0400

    App.jsx initial setup structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
-import React, { useState } from 'react';
-import Gallery from './components/Gallery';
+import React, { useState } from 'react'; // Importing React and useState
+import Gallery from './components/Gallery'; //  Importing Gallery componen
+import DestinationSelector from './components/DestinationSelector'; // Importing DestinationSelector component
 
 const App = () => {
   const [tours, setTours] = useState([]);
+  const { selectedDestination, setSelectedDestination } = useState('All');
 
   const removeTour = (id) => {
     setTours((prevTours) => prevTours.filter((tour) => tour.id !== id));
   };
 
+  const filteredTours = 
+    selectedDestination === 'All'
+      ? tours
+      : tours.filter((tour) => tour.destination === selectedDestination); 
+
   return (
     <main>
       <h1>Tour Comparison</h1>
+      <DestinationSelector
+        selectedDestination={selectedDestination}
+        setSelectedDestination={setSelectedDestination}
+      />
+      <h2>Available Tours</h2>
       <Gallery tours={tours} setTours={setTours} onRemove={removeTour} />
     </main>
   );
