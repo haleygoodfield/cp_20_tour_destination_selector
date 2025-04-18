7c3a6c6462adce4e52634835c1ef759c9ebaf5b5
Haley Goodfield <haleygoodfield@usf.edu>
2025-04-18 09:51:25 -0400

    TourCard.jsx for challenge instructions

--- src/components/TourCard.jsx
+++ src/components/TourCard.jsx
@@ -1,28 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'; // Importing React
 
-const TourCard = ({ id, image, info, price, name, onRemove }) => {
-  const [readMore, setReadMore] = useState(false);
+const TourCard = ({ id, image, info, price, name, onRemove }) => { // TourCard component
+  const [readMore, setReadMore] = useState(false); // State to toggle read more/less
 
   return (
-    <article className="tour-card">
-      <img src={image} alt={name} />
+    // TourCard component structure
+    <article className="tour-card"> 
+      <img src={image} alt={name} /> {/* Image of the tour */}
       <footer>
-        <div className="tour-info">
+        <div className="tour-info"> {/* Tour information */}
           <h4>{name}</h4>
-          <h4 className="tour-price">${price}</h4>
+          <h4 className="tour-price">${price}</h4> {/* Price of the tour */}
         </div>
         <p>
-          {readMore ? info : `${info.substring(0, 150)}...`}
-          <button onClick={() => setReadMore(!readMore)}>
-            {readMore ? 'Show Less' : 'Read More'}
+          {readMore ? info : `${info.substring(0, 150)}...`} {/* Tour info with read more functionality */}
+          <button onClick={() => setReadMore(!readMore)}> {/* Toggle read more/less */}
+            {readMore ? 'Show Less' : 'Read More'} 
           </button>
         </p>
-        <button className="btn" onClick={() => onRemove(id)}>
-          Not Interested
+        <button className="btn" onClick={() => onRemove(id)}> {/* Remove tour button */}
+          Not Interested {/* Button to remove the tour */}
         </button>
       </footer>
     </article>
   );
 };
 
-export default TourCard;
+export default TourCard; // TourCard component
